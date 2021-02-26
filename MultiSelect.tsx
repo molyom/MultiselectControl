9c30a97380cf607321d923ffb28c52755108f2d4
MY Dynamics <moly_omidvar@hotmail.es>
2021-02-26 11:13:12 +0100

    Initial commit

---
+++ MultiSelect.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import AsyncSelect from 'react-select/async';
+
+export interface IProps {
+	displayValueField: any;
+	displayFieldLabel: any;
+	columns: any;
+	topCount: any;
+	filterField: any;
+	entityName: any;
+    value: string;
+	onChange: (value:string) => void;
+	onSearch: (value:string) => void;
+	records: any
+}
+
+export interface IState {
+    value: string;
+}
+
+export class MultiSelectControl extends React.Component<IProps, IState> {
+			
+    constructor(props: Readonly<IProps>) {
+		super(props);
+		this.state = { value: props.value};     
+    }
+
+	componentWillReceiveProps(p: IProps) 
+	{
+		this.setState({value : (p.value)});
+		console.log("react props");
+    }
+
+	onChange = (ob: any) =>
+	{
+		if (ob == null) {
+			this.setState({value : "" });
+			this.props.onChange("");
+			return;
+		};
+
+		var res = ob.map((e: any) => e[this.props.displayValueField]).join(",");
+
+		this.setState({value : res });
+		this.props.onChange(res);
+	}
+
+	loadOptions = async (inputValue: string) => {
+		const res = this.props.onSearch(inputValue);
+		console.log("returning" + JSON.stringify(res));
+		return res;		
+	}	
+	
+	public render(): JSX.Element 
+	{		
+		const selectStyles = { menuPortal: (zindex: any) => ({ ...zindex, zIndex: 9999}) };
+
+        return (
+			<div>
+			<AsyncSelect
+			isMulti={true}
+			menuPortalTarget={document.body}
+			styles={selectStyles}
+			getOptionLabel={e => e[this.props.displayFieldLabel]}
+			getOptionValue={e => e[this.props.displayValueField]}
+			loadOptions={this.loadOptions}
+			defaultOptions
+			onChange={this.onChange}		
+			/></div>
+        );
+	}
+}
---
+++ generated/ManifestTypes.d.ts
@@ -0,0 +1,17 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    sampleProperty: ComponentFramework.PropertyTypes.StringProperty;
+    entityName: ComponentFramework.PropertyTypes.StringProperty;
+    filterField: ComponentFramework.PropertyTypes.StringProperty;
+    topCount: ComponentFramework.PropertyTypes.StringProperty;
+    columns: ComponentFramework.PropertyTypes.StringProperty;
+    displayFieldLabel: ComponentFramework.PropertyTypes.StringProperty;
+    displayValueField: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    sampleProperty?: string;
+}
